d99a537da86e79d7109fb0ab0359a50a937624db
maimai <yule919@gmail.com>
2024-11-30 18:03:49 -0700

    fix: CORS issue

---
+++ src/main/java/com/maimai/nlsearch/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.maimai.nlsearch.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfig implements WebMvcConfigurer {
+
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(allowedOrigins.split(","))
+                .allowedMethods("POST")
+                .allowCredentials(true);
+    }
+}
